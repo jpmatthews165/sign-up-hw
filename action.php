4841ab14aa7fe9792d2f72e77aa3f9c1c88bd273
Josh Matthews <jm775@afsconnect2.njit.edu>
2018-03-20 11:27:19 -0400

    init sign-up upload

---
+++ action.php
@@ -0,0 +1,16 @@
+<?php
+require "db.php" ;
+
+$fname= $_GET["fname"];
+$lname= $_GET["lname"];
+$email= $_GET["email"];
+$phone= $_GET["phone"];
+$birthday= $_GET["birthday"];
+$gender= $_GET["gender"];
+
+$sql = "insert into accounts (email, fname, lname,phone, birthday,
+		gender) values ('$fname', '$lname', '$email', '$phone', '$birthday', '$gender');";
+$results = runQuery($sql);
+header("Location: index.html");
+
+?>
